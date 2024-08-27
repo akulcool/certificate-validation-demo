65b008c7871daf6872c4ee57807804b8fb43fb10
Akul Arora <akularora@Akuls-MacBook-Air.local>
2024-08-27 13:32:06 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
+  const isSecure = window.location.protocol === 'https:';
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Certificate Validation Demo</h1>
+        {isSecure ? (
+          <p>The site is secure (using HTTPS).</p>
+        ) : (
+          <p>The site is not secure (using HTTP).</p>
+        )}
       </header>
     </div>
   );
 }
 
 export default App;
+
